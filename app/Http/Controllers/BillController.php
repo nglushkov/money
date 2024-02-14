58ced1d4d039be4198176686258f77905766d9ed
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-02-14 03:03:41 +0000

    Fix bill cache

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -114,6 +114,7 @@ class BillController extends Controller
                     'amount' => $amount
                 ]);
                 $billCurrency->save();
+                $bill->clearAmountCache();
             }
         });
 
