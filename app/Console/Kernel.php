40fc93dabc3a675b49f8338bee89164758304345
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-02-15 01:29:46 +0000

    Fix Command rates:get

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('rates:get-usd-ars')->evertyThreeHours();
+        $schedule->command('rates:get-usd-ars')->everyThreeHours();
         $schedule->command('rates:get')->everyThreeHours();
     }
 
