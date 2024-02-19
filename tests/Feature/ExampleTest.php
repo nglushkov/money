cd2dafc696c73ebe04614a42a3cbea5331eb2559
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-02-19 11:52:01 -0300

    Fix Feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,16 +3,22 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $this->seed();
+        logger()->info('test_the_application_returns_a_successful_response');
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get('/');
 
         $response->assertStatus(200);
     }
