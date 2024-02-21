eb3d218abea63c2843c05154b98fa61a65466648
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-02-21 16:27:14 -0300

    Change delete-link view

--- resources/views/blocks/delete-link.blade.php
+++ resources/views/blocks/delete-link.blade.php
@@ -1,6 +1,7 @@
-<a href="{{ route($routePart . '.edit', $model) }}" class="card-link">Редактировать</a>
+<a href="{{ route($routePart . '.edit', $model) }}" class="card-link">Edit</a>
+<a href="{{ route($routePart . '.index') }}" class="card-link">Show All</a>
 <a href="#" class="card-link link-danger"
-    onclick="event.preventDefault(); if (confirm('Are you sure you want to delete?')) { document.getElementById('delete-form').submit(); }">Удалить</a>
+    onclick="event.preventDefault(); if (confirm('Are you sure you want to delete?')) { document.getElementById('delete-form').submit(); }">Remove</a>
 <form id="delete-form" action="{{ route($routePart . '.destroy', $model) }}" method="POST" style="display: none;">
     @csrf
     @method('DELETE')
