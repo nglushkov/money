f0f77dcdc95e788e0b606b0bb1d55f16a89f6d58
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-03-08 18:42:15 -0300

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        //User::factory(10)->create();
         User::create([
             'name' => 'Default User',
             'email' => 'default@example.com',
