d89561b74f89e221cbe1bb5d661c33107f713c8a
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-04-06 13:50:21 -0300

    Change Bills cache clearing

--- app/Listeners/ClearBillAmountCache.php
+++ app/Listeners/ClearBillAmountCache.php
@@ -3,8 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\MoveProcessed;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
+use App\Models\Bill;
 use Illuminate\Support\Facades\Log as Monolog;
 
 class ClearBillAmountCache
@@ -21,9 +20,10 @@ class ClearBillAmountCache
      */
     public function handle(MoveProcessed $event): void
     {
-        $relatedBills = $event->move->getRelatedBills();
+        // @todo: Refactor. Problem with previous changed unrelated bills
+//        $relatedBills = $event->move->getRelatedBills();
         Monolog::info('MoveProcessed event handled', ['id' => $event->move->id, 'type' => get_class($event->move)]);
-        foreach ($relatedBills as $bill) {
+        foreach (Bill::all() as $bill) {
             $bill->clearAmountCache();
             Monolog::info('Bill amount cache cleared for bill', ['id' => $bill->id, 'name' => $bill->name]);
         }
