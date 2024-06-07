8a27112caff6475f3d3e80d127f1637fd2226479
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-06-07 13:46:29 -0300

    Add command to get crypto rates

--- app/Console/Commands/GetCryptoRates.php
+++ app/Console/Commands/GetCryptoRates.php
@@ -2,7 +2,10 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Currency;
+use App\Models\Rate;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
 
 class GetCryptoRates extends Command
 {
@@ -18,13 +21,43 @@ class GetCryptoRates extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Fetch the latest cryptocurrency rates from CoinMarketCap';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        // Замените 'your-api-key' на ваш ключ API CoinMarketCap
+        $apiKey = env('COINMARKETCAP_API_KEY');
+
+        $symbolsStrings = Currency::where('is_crypto', true)
+            ->where('id', '!=', Currency::getDefaultCurrencyId(true))
+            ->pluck('name')->implode(',');
+
+        $response = Http::withHeaders([
+            'Accepts' => 'application/json',
+            'X-CMC_PRO_API_KEY' => $apiKey,
+        ])->get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest', [
+            'symbol' => $symbolsStrings,
+            'convert' => Currency::getDefaultCurrencyName(true),
+        ]);
+
+        if ($response->successful()) {
+            $data = $response->json();
+            foreach ($data['data'] as $crypto) {
+                $rateInverted = 1 / $crypto['quote']['USDT']['price'];
+                $rate = Rate::firstOrNew([
+                    'from_currency_id' => Currency::getDefaultCurrencyId(true),
+                    'to_currency_id' => Currency::where('name', $crypto['symbol'])->first()->id,
+                    'rate' => $rateInverted,
+                    'date' => now(),
+                ]);
+                $rate->save();
+                logger()->info('Fetched rate for ' . $crypto['symbol'] . ' at ' . $rate->rate);
+            }
+        } else {
+            $this->error('Failed to fetch cryptocurrency rates.');
+        }
     }
 }
