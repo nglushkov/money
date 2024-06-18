ce232fa4a9e45852fd0218cd7fd81df5f1b80879
Nikita Glushkov <glushkov.nik1@gmail.com>
2024-06-18 18:14:42 -0300

    Change transfers form

--- resources/views/transfers/create.blade.php
+++ resources/views/transfers/create.blade.php
@@ -19,43 +19,60 @@
                         </div>
                     @endif
                     @csrf
-                    <div class="form-group mb-2">
-                        <label for="from_bill_id">From Bill</label>
-                        <select name="from_bill_id" id="from_bill_id" class="form-control" required>
-                            <option value="">Select Bill</option>
-                            @foreach ($bills as $bill)
-                                <option value="{{ $bill->id }}" @selected(old('from_bill_id') == $bill->id)>{{ $bill->name_with_user }}</option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="from_bill_id">From:</label>
+                        </div>
+                        <div class="col-6">
+                            <select name="from_bill_id" id="from_bill_id" class="form-control" required>
+                                @foreach ($bills as $bill)
+                                    <option value="{{ $bill->id }}" @selected(old('from_bill_id') == $bill->id)>{{ $bill->name_with_user }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="to_bill_id">To Bill</label>
-                        <select name="to_bill_id" id="to_bill_id" class="form-control" required>
-                            <option value="">Select Bill</option>
-                            @foreach ($bills as $bill)
-                                <option value="{{ $bill->id }}" @selected(old('to_bill_id') == $bill->id)>{{ $bill->name_with_user }}</option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="to_bill_id">To:</label>
+                        </div>
+                        <div class="col-6">
+                            <select name="to_bill_id" id="to_bill_id" class="form-control" required>
+                                @foreach ($bills as $bill)
+                                    <option value="{{ $bill->id }}" @selected(old('to_bill_id') == $bill->id)>{{ $bill->name_with_user }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="amount">Amount</label>
-                        <input type="text" name="amount" id="amount" class="form-control" required value="{{ old('amount') }}">
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="amount">Amount:</label>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" name="amount" id="amount" class="form-control" required value="{{ old('amount') }}">
+                        </div>
+                        <div class="col-auto">
+                            <select name="currency_id" id="currency_id" class="form-control" required>
+                                @foreach ($currencies as $currency)
+                                    <option value="{{ $currency->id }}" @selected(old('currency_id') == $currency->id)>{{ $currency->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="currency_id">Currency</label>
-                        <select name="currency_id" id="currency_id" class="form-control" required>
-                            @foreach ($currencies as $currency)
-                                <option value="{{ $currency->id }}" @selected(old('currency_id') == $currency->id)>{{ $currency->name }}</option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="date">Date:</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="date" name="date" id="date" class="form-control" value="{{ old('date', date('Y-m-d')) }}" required>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="date">Date</label>
-                        <input type="date" name="date" id="date" class="form-control" value="{{ old('date', date('Y-m-d')) }}" required>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="notes">Notes</label>
-                        <input type="text" name="notes" id="notes" class="form-control" value="{{ old('notes') }}">
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="notes">Notes:</label>
+                        </div>
+                        <div class="col">
+                            <input type="text" name="notes" id="notes" class="form-control" value="{{ old('notes') }}">
+                        </div>
                     </div>
                     <hr>
                     <button type="submit" class="btn btn-primary">Create</button>
--- resources/views/transfers/edit.blade.php
+++ resources/views/transfers/edit.blade.php
@@ -13,56 +13,67 @@
                         <div class="alert alert-danger">
                             <ul>
                                 @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
+                                <li>{!! $error !!}</li>
                                 @endforeach
                             </ul>
                         </div>
                     @endif
                     @csrf
                     @method('PUT')
-                    <div class="form-group mb-2">
-                        <label for="from_bill_id">From Bill</label>
-                        <select name="from_bill_id" id="from_bill_id" class="form-control" required>
-                            @foreach ($bills as $bill)
-                                <option value="{{ $bill->id }}" @selected(old('from_bill_id') == $bill->id or $transfer->from_bill_id == $bill->id)>
-                                    {{ $bill->name_with_user }}
-                                </option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="from_bill_id">From:</label>
+                        </div>
+                        <div class="col-auto">
+                            <select name="from_bill_id" id="from_bill_id" class="form-control" required>
+                                @foreach ($bills as $bill)
+                                    <option value="{{ $bill->id }}" @selected(old('from_bill_id') == $bill->id || $transfer->from_bill_id == $bill->id)>{{ $bill->name_with_user }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="to_bill_id">To Bill</label>
-                        <select name="to_bill_id" id="to_bill_id" class="form-control" required>
-                            @foreach ($bills as $bill)
-                                <option value="{{ $bill->id }}" @selected(old('to_bill_id') == $bill->id or $transfer->to_bill_id == $bill->id)>
-                                    {{ $bill->name_with_user }}
-                                </option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="to_bill_id">To:</label>
+                        </div>
+                        <div class="col-auto">
+                            <select name="to_bill_id" id="to_bill_id" class="form-control" required>
+                                @foreach ($bills as $bill)
+                                    <option value="{{ $bill->id }}" @selected(old('to_bill_id') == $bill->id || $transfer->to_bill_id == $bill->id)>{{ $bill->name_with_user }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="amount">Amount</label>
-                        <input type="text" name="amount" id="amount" class="form-control" value="{{ $transfer->amount }}"
-                            required>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="amount">Amount:</label>
+                        </div>
+                        <div class="col-6">
+                            <input type="text" name="amount" id="amount" class="form-control" required value="{{ old('amount', $transfer->amount) }}">
+                        </div>
+                        <div class="col-auto">
+                            <select name="currency_id" id="currency_id" class="form-control" required>
+                                @foreach ($currencies as $currency)
+                                    <option value="{{ $currency->id }}" @selected(old('currency_id') == $currency->id || $transfer->currency_id == $currency->id)>{{ $currency->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="currency_id">Currency</label>
-                        <select name="currency_id" id="currency_id" class="form-control" required>
-                            @foreach ($currencies as $currency)
-                                <option value="{{ $currency->id }}" @selected(old('currency_id') == $currency->id or $transfer->currency_id == $currency->id)>
-                                    {{ $currency->name }}
-                                </option>
-                            @endforeach
-                        </select>
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="date">Date:</label>
+                        </div>
+                        <div class="col-auto">
+                            <input type="date" name="date" id="date" class="form-control" value="{{ old('date', $transfer->date->format('Y-m-d')) }}" required>
+                        </div>
                     </div>
-                    <div class="form-group mb-2">
-                        <label for="date">Date</label>
-                        <input type="date" name="date" id="date" class="form-control" value="{{ old('date', $transfer->date->format('Y-m-d')) }}"
-                            required>
-                    </div>
-                    <div class="form-group mb-2">
-                        <label for="notes">Notes</label>
-                        <input type="text" name="notes" id="notes" class="form-control" value="{{ old('notes', $transfer->notes) }}">
+                    <div class="row g-3 align-items-center mb-2">
+                        <div class="col-2">
+                            <label for="notes">Notes:</label>
+                        </div>
+                        <div class="col">
+                            <input type="text" name="notes" id="notes" class="form-control" value="{{ old('notes', $transfer->notes) }}">
+                        </div>
                     </div>
                     <hr>
                     <button type="submit" class="btn btn-primary">Update</button>
